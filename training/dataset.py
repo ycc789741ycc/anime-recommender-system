d2a50f28ef75dbe80504ca3a591cd0400f56d746
yoshi <yoshi4868686@gmail.com>
2022-06-20 09:23:26 +0800

    Add method to handle large file.

---
+++ training/dataset.py
@@ -0,0 +1,17 @@
+import torch
+import pandas as pd
+from torch.utils.data import Dataset
+
+
+class MyDataSet(Dataset):
+    def __init__(self, df_x: pd.DataFrame, df_y: pd.DataFrame) -> None:
+        super().__init__()
+        self.df_x = df_x
+        self.df_y = df_y
+        assert len(self.df_x) == len(self.df_y), "The length of x and y should be same."
+    
+    def __len__(self):
+        return len(self.df_x)
+    
+    def __getitem__(self, index):
+        return torch.LongTensor(self.df_x.iloc[index].values), torch.FloatTensor(self.df_y.iloc[index].values)        
