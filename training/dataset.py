abd6b0e501fe253ebf03a8009ee012b555666b89
Yoshi <yoshi.gao@aiello.ai>
2022-06-20 11:14:19 +0800

    Modify the MyDataset.

--- training/dataset.py
+++ training/dataset.py
@@ -6,8 +6,8 @@ from torch.utils.data import Dataset
 class MyDataSet(Dataset):
     def __init__(self, df_x: pd.DataFrame, df_y: pd.DataFrame) -> None:
         super().__init__()
-        self.df_x = df_x
-        self.df_y = df_y
+        self.df_x = df_x.copy(deep=False)
+        self.df_y = df_y.copy(deep=False)
         assert len(self.df_x) == len(self.df_y), "The length of x and y should be same."
     
     def __len__(self):
