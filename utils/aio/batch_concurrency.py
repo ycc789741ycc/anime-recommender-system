349df94fc8b7ba82b3cb67ffb207463422d68eab
yoshi <yoshi4868686@gmail.com>
2022-07-16 15:23:05 +0800

    Add the model and data file.

--- utils/aio/batch_concurrency.py
+++ utils/aio/batch_concurrency.py
@@ -5,24 +5,24 @@ from typing import Any, Coroutine, List
 class AioBatchConcurrency():
     """
     Seperating the coroutines into batches and run each batch coroutines individually.
-    
+
     Example:
     >>> couroutines = [test1(), test2(), ...]
     >>> concurrency_limiter = AioBatchConcurrency(batch_size=10)
     >>> results = await concurrency_limiter.run_batch_concurrency(
         coroutines=couroutines
         )
-    
-    """  
+
+    """
     def __init__(self, batch_size) -> None:
         self.semaphore = asyncio.Semaphore(batch_size)
-        
+
     async def run_batch_concurrency(self, coroutines: List[Coroutine]) -> List[Any]:
         async def semaphore_wrapper(coroutine: Coroutine) -> Coroutine:
             async with self.semaphore:
                 result = await coroutine
             return result
-        
+
         futures = []
         results = []
         for coroutine in coroutines:
